78088c76d78c9b91cef3114179eded271c61fa48
Peter Nehrer <peter.nehrer@pubnub.com>
2020-04-08 20:22:48 -0400

    Try static linkage

--- build.rs
+++ build.rs
@@ -5,19 +5,29 @@ fn main() {
         }
     }
 
-    println!("cargo:rustc-flags=-l dylib=cassandra");
-    println!("cargo:rustc-flags=-l dylib=crypto");
-    println!("cargo:rustc-flags=-l dylib=ssl");
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-flags=-l dylib=c++");
-    #[cfg(not(target_os = "macos"))]
-    println!("cargo:rustc-flags=-l dylib=stdc++");
-    println!("cargo:rustc-flags=-l dylib=uv");
-    println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib64");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib");
-    println!("cargo:rustc-link-search={}", "/usr/lib64/");
-    println!("cargo:rustc-link-search={}", "/usr/lib/");
-    println!("cargo:rustc-link-search={}", "/usr/local/opt/openssl/lib");
+    #[cfg(all(target_arch = "x86_64", target_os = "linux", target_env = "musl"))]
+    {
+        println!("cargo:rustc-link-lib=static=cassandra");
+        println!("cargo:rustc-link-lib=static=crypto");
+        println!("cargo:rustc-link-lib=static=ssl");
+        println!("cargo:rustc-link-lib=static=stdc++");
+        println!("cargo:rustc-link-lib=static=uv");
+        println!("cargo:rustc-link-search={}", "/usr/local/musl/lib");
+    }
+
+    // println!("cargo:rustc-flags=-l dylib=cassandra");
+    // println!("cargo:rustc-flags=-l dylib=crypto");
+    // println!("cargo:rustc-flags=-l dylib=ssl");
+    // #[cfg(target_os = "macos")]
+    // println!("cargo:rustc-flags=-l dylib=c++");
+    // #[cfg(not(target_os = "macos"))]
+    // println!("cargo:rustc-flags=-l dylib=stdc++");
+    // println!("cargo:rustc-flags=-l dylib=uv");
+    // println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
+    // println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
+    // println!("cargo:rustc-link-search={}", "/usr/local/lib64");
+    // println!("cargo:rustc-link-search={}", "/usr/local/lib");
+    // println!("cargo:rustc-link-search={}", "/usr/lib64/");
+    // println!("cargo:rustc-link-search={}", "/usr/lib/");
+    // println!("cargo:rustc-link-search={}", "/usr/local/opt/openssl/lib");
 }
